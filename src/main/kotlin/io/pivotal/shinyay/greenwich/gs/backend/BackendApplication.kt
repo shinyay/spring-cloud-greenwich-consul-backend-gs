3d1a79388189c6441eddb2a65a98f8bb76cd47b5
shinyay <shinya.com@gmail.com>
2019-08-16 10:43:45 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/greenwich/gs/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.greenwich.gs.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
