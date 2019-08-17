8db2613150a90bc7dd4954884f1e521bbc15cea8
shinyay <shinya.com@gmail.com>
2019-08-17 17:43:47 +0900

    Add controller to show config data from Consul

---
+++ src/main/kotlin/io/pivotal/shinyay/greenwich/gs/backend/controller/BackendController.kt
@@ -0,0 +1,14 @@
+package io.pivotal.shinyay.greenwich.gs.backend.controller
+
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/backend")
+class BackendController (val environment: Environment){
+
+    @GetMapping
+    fun hello() = "Hello, Backend service on port: ${environment.getProperty("local.server.port")}"
+}
